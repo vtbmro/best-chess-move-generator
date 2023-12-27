b74b2ca1fd65eff3f9b2d416d85331c4d71a8209
Vicente <vt.bmro@gmail.com>
2023-12-27 21:59:54 +0100

    todo

--- move_generator/best_move_generator/models.py
+++ move_generator/best_move_generator/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+# TODO: allows users to click and drag to add pieces to the board
+# TODO: click and drag pieces out of the board to clear them from 
+# the table
+# TODO: add button to make a starting position board
+# TODO: add a button to clear the board.
+# TODO: Calculate best move button
+
