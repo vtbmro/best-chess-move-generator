82f2fd63eca8ea0ab2d7736f65d28e3d393c1d60
Vicente <vt.bmro@gmail.com>
2024-01-04 11:54:58 +0100

    made url chessboardjs to  load imgs

--- best_move_generator/urls.py
+++ best_move_generator/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name = "index")
+    path("", views.index, name = "index"),
+
+    # Necessary path for chessboard.js to work with Django
+    path("img/chesspieces/wikipedia/<str:piece>", views.piece_png, name="piece_png")
 ]
