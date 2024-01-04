af2678f9beb8cc4da596166326550f39fd54b74e
Vicente <vt.bmro@gmail.com>
2024-01-04 11:55:13 +0100

    added function to load images chessboardjs

--- best_move_generator/views.py
+++ best_move_generator/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 def index(request):
     return render(request, "best_move_generator/index.html")
+
+# Function that allows the images to load
+def piece_png(request, piece):
+    return redirect(f"https://chessboardjs.com/img/chesspieces/wikipedia/{piece}")
