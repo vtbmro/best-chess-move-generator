fec778c7a6c75d362ff1fca414d4ee2252757d5d
Vicente <vt.bmro@gmail.com>
2024-01-08 21:44:28 +0100

    working on API

--- best_move_generator/views.py
+++ best_move_generator/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect
+from .eval import evaluate 
+from .search import minimax_alphabeta
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -8,3 +11,21 @@ def index(request):
 # Function that allows the images to load
 def piece_png(request, piece):
     return redirect(f"https://chessboardjs.com/img/chesspieces/wikipedia/{piece}")
+
+# API that calculates the best possible move
+def calculate_best_move(request, fen):
+
+# Check if request is GET
+    if request.method != "GET":
+        return JsonResponse({"error": "GET request required."}, status=400)
+
+# If request is GET return best move from fen        
+    else:
+
+# Transform into valid fen
+        fen = fen.replace("_","/")
+
+# TODO: get input from player wheter it is white to play or black to play
+
+
+        return JsonResponse({"current board fen":f"{fen}"})
