c75774f53130c3a7c480935b9e1700300d7a13f6
Vicente <vt.bmro@gmail.com>
2024-01-09 19:18:34 +0100

    add color choice url

--- best_move_generator/urls.py
+++ best_move_generator/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path("img/chesspieces/wikipedia/<str:piece>", views.piece_png, name="piece_png"),
 
     # API
-    path("calculate_best_move/<str:fen>" ,views.calculate_best_move, name="calculate_best_move")
+    path("calculate_best_move/<str:fen>/<str:moves_next>" ,views.calculate_best_move, name="calculate_best_move")
 ]
