c5b741d6ca15e3819bde49a6ba18a8085a4b4498
Vicente <vt.bmro@gmail.com>
2024-01-10 16:21:00 +0100

    added a way for the user to chose what color moves next

--- best_move_generator/models.py
+++ best_move_generator/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 # Create your models here.
 
-# TODO: allow the user to input what side has to move next black/white
 
-# TODO: Button that calculates best move
 
-# TODO: refuse impossible positions  example: two kings
+
+# TODO: add backend for invalid FENS (Anyboard that is different from 1 king of each color)
+# TODO: cases if user doesnt chose a color to move
+# TODO: styling
+
 
