b7fcbb68142a4e81804c1ead5e3c5a96ad1beecf
Vicente <vt.bmro@gmail.com>
2024-01-15 10:14:46 +0100

    finished models

--- best_move_generator/models.py
+++ best_move_generator/models.py
@@ -1,12 +1,6 @@
 from django.db import models
 
-# Create your models here.
-
-
-
-
-# TODO: add backend for invalid FENS (Anyboard that is different from 1 king of each color)
-# TODO: cases if user doesnt chose a color to move
-# TODO: styling
-
+# Note: the pa
+class User(AbstractUser):
+    pass
 
