076149a78a8bd38d513dba251a0ab80459156343
Vicente <vt.bmro@gmail.com>
2024-01-15 10:14:57 +0100

    models

--- best_move_generator/models.py
+++ best_move_generator/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Note: the pa
+# Note: the page doesn't need users to work but since one of the requi
+# rements is that page uses at least one model here it is.
+
 class User(AbstractUser):
     pass
 
